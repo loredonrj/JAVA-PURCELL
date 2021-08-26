a17dca97630c2ee408460b1d05c00a405786139e
loredonrj <loredonrj@users.noreply.github.com>
2021-08-26 19:18:49 -0400

    Update App.java

--- Purcell.Complete.Beginner.28.Interfaces/App.java
+++ Purcell.Complete.Beginner.28.Interfaces/App.java
@@ -10,6 +10,7 @@ person1.greet();
 
 Info info1 = new Machine(); // Illustrate possible usage of Info Interface as a Type, like a Class
 info1.showInfo(); // the created object of Type Machine is assigned to a variable of type Info, and the method in Info Interface can be called on it
+  // which is really cool
 }
 
 }
